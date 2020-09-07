f3533ee525d6317541f522fd7de648a9ea15916f
LaZsolt <60486174+LaZsolt@users.noreply.github.com>
2020-09-07 20:32:44 +0200

    Delete Arduino.h

--- for_MCUdude/funcion/Arduino.h
+++
@@ -1,7 +0,0 @@
-#define clockCyclesPerMicrosecond()  ( F_CPU / 1000000L )
-#define clockCyclesToMicroseconds(a) ( ((a) * 1000L) / (F_CPU / 1000L) )
-#define microsecondsToClockCycles(a) ( ((a) * (F_CPU / 1000L)) / 1000L )
-
-...
-
-void delayMicroseconds(unsigned int us) __attribute__ ((noinline));
