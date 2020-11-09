d69558ffd3243f25e4b7aa5f2ce48203d9d7f87d
LaZsolt <60486174+LaZsolt@users.noreply.github.com>
2020-11-09 19:05:24 +0100

    Update Arduino.h

--- for_LGT8F/Arduino.h
+++ for_LGT8F/Arduino.h
@@ -4,7 +4,7 @@
     } else {                        \
         delayMicroseconds_v(us);    \
     }
-#define delayMicroseconds_c(_us) _lgt8fx_delay_cycles( (uint32_t)( ((double)(F_CPU/1000)*us)/1000) ) // for constant case
+#define delayMicroseconds_c(us) _lgt8fx_delay_cycles((uint32_t)(  (F_CPU/1e6)*((double)us)  )) // for constant case
  void   delayMicroseconds_v(unsigned int us) __attribute__ ((noinline));         // for variable case which is same as earlier
 
 __inline__ void _lgt8fx_delay_cycles(const uint32_t cticks) __attribute__((always_inline));
